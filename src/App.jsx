e4dc3bf6eb4327b505d629ab63386929a3f8591c
bismay70 <samalbismay5@gmail.com>
2025-08-24 23:33:01 +0530

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home.jsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Welcome to ML4E homepage</h1>
-        <p className="mt-2">Your content here...</p>
-      </div>
+      <Home />
     </>
   );
 }
