c8e2e6668d96ad497802a0ddb7854a79c640268b
Ashwani Senapati <senapatiashwani47@gmail.com>
2025-08-29 11:44:38 +0530

    chore:enhanced ui of hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home.jsx";
-
+import Hero3D from "./components/Home";
 export default function App() {
   return (
     <>
       <Navbar />
       <Home />
+      {/* <Hero3D /> */}
     </>
   );
 }
