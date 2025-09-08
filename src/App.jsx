8bd2ceff19e4d0986f2bb844472f903a5222bda7
Ashwani Senapati <senapatiashwani47@gmail.com>
2025-09-08 22:25:27 +0530

    fix: center

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/Home.jsx";
+import Home from "./components/Home";
 import Hero3D from "./components/Home";
 export default function App() {
   return (
     <>
-      <Navbar />
+      
       <Home />
       {/* <Hero3D /> */}
     </>
